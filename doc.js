fa1d628ff6783f3ccd5709994b6b767132ec96c4
João Henrique Xavier <jhcxavier@gmail.com>
2019-10-24 22:57:12 +0000

    finished

--- doc.js
+++ doc.js
@@ -1,18 +1,18 @@
-window.onload=()=>{
-button = () =>{
-let who = ['The dog ','My granma ','His turtle ','My bird '];
-let what = ['eat ','pissed ','crushed ','broked '];
-let when = ['before the class.','right in time.','when I finished.','during my lunch.'];
+window.onload = () => {
+    button = () => {
+        let who = ['The dog ', 'My granma ', 'His turtle ', 'My bird '];
+        let what = ['eat ', 'pissed ', 'crushed ', 'broked '];
+        let when = ['before the class.', 'right in time.', 'when I finished.', 'during my lunch.'];
 
-// function randEx( arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-// }
+        // function randEx( arr) {
+        //     return arr[Math.floor(Math.random() * arr.length)];
+        // }
 
-let who2 = Math.floor(Math.random() * who.length);
-let what2 = Math.floor(Math.random() * what.length);
-let when2 = Math.floor(Math.random() * when.length);
-document.querySelector('#excuses').innerHTML = who[who2] + what[what2] + when[when2];
-}
+        let who2 = Math.floor(Math.random() * who.length);
+        let what2 = Math.floor(Math.random() * what.length);
+        let when2 = Math.floor(Math.random() * when.length);
+        document.querySelector('#excuses').innerHTML = who[who2] + what[what2] + when[when2];
+    }
 }
 // function excuse() {
 //     // let exc = "";
